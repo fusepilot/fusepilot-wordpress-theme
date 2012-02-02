3e119f350e9db3ed0e83bda0042886d5ca334a81
Michael Delaney <fusepilot@gmail.com>
2012-02-02 10:13:37 -0600

    removed robots meta plugin cuz suckage.

--- plugins/robots-meta/robots-meta-admin.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function () {
-	jQuery("#disableexplanation").change(function() {
-		if (jQuery("#disableexplanation").is(':checked')) {
-			jQuery("p.desc").css("display","none");
-		} else {
-			jQuery("p.desc").css("display","block");
-		}
-	}).change();
-});
